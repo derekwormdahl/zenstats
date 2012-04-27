7769784b8df2e1b90e622c55e7cd36e967953150
U-AD\derek <derek@IS-L-R8KG2TN.ad.uoregon.edu>
2012-04-27 15:56:10 -0700

    update

---
+++ python/zenstats/zenstats.py
@@ -0,0 +1,14 @@
+import web
+import google-analytics/zen_analytics
+
+urls = (
+    '/', 'index'
+)
+
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
